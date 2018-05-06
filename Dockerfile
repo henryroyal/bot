59aeed36e1299d95b85ec1f45b156787713c8471
henry <dev@hwr.io>
2018-05-05 22:14:58 -0400

    ssh client stuff, related config, one basic command

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,20 @@
 FROM golang:1.10-alpine
 
+ENV BOT_NAME chatty
+ENV BOT_VERSION 0.0.1
+ENV BOT_HOST www.hwr.io
+ENV BOT_PORT 2022
+
+ENV BOT_PRIVATE_KEY ./.ssh/id_rsa
+ENV HOST_PUBLIC_KEY ./.ssh/id_rsa.host.pub
+ENV ALLOW_INSECURE_HOSTKEY false
+
+ENV HISTORY_PLAYBACK_LEN 20
+
 WORKDIR /go/src/app
 COPY . .
 
 RUN go get -d -v .
 RUN go install -v .
-RUN go build -o /usr/local/bin/bot main.go
 
-CMD ["/usr/local/bin/bot"]
+CMD ["/go/bin/app"]
--- main.go
+++ main.go
@@ -6,39 +6,128 @@ import (
 	"log"
 	"fmt"
 	"bufio"
-	"net"
 	"crypto/rsa"
 	"crypto/rand"
+	"io/ioutil"
+	"strings"
+	"errors"
+	"strconv"
+	"time"
 )
 
-const (
-	NAME   string = "chatty"
-	SERVER string = "www.hwr.io"
-	PORT   int    = 2022
+var (
+	// Environment Variables
+	BOTNAME string = os.Getenv("BOT_NAME")
+	VERSION string = os.Getenv("BOT_VERSION")
+	HOST    string = os.Getenv("BOT_HOST")
+	PORT    string = os.Getenv("BOT_PORT")
+
+	BOT_PRIVATE_KEY string = os.Getenv("BOT_PRIVATE_KEY")
+	HOST_PUBLIC_KEY string = os.Getenv("HOST_PUBLIC_KEY")
+
+	ALLOW_INSECURE_HOSTKEY string = os.Getenv("ALLOW_INSECURE_HOSTKEY")
+	HISTORY_PLAYBACK_LEN   string = os.Getenv("HISTORY_PLAYBACK_LEN")
+
+	// Errors
+	InsecureHostkeyError error = errors.New("Set ALLOW_INSECURE_HOSTKEY=true to allow ssh.InsecureIgnoreHostKey callback")
 )
 
-func main() {
-	key, err := rsa.GenerateKey(rand.Reader, 2014)
-	if err != nil {
-		log.Fatalln("failed to generate RSA key")
+func getHostPubkeyCallback(hostPublicKeyPath string, allowInsecureHostkey bool) (ssh.HostKeyCallback, error) {
+
+	if hostPublicKeyPath == "" && allowInsecureHostkey == false {
+		return nil, InsecureHostkeyError
 	}
 
-	signer, err := ssh.NewSignerFromKey(key)
+	if hostPublicKeyPath == "" && allowInsecureHostkey == true {
+		return ssh.InsecureIgnoreHostKey(), nil
+	}
+
+	keyBytes, err := ioutil.ReadFile(HOST_PUBLIC_KEY)
 	if err != nil {
-		log.Fatalf("unable to parse private key: %v", err)
+		return nil, err
+	}
+
+	hostkey, _, _, _, err := ssh.ParseAuthorizedKey(keyBytes)
+	if err != nil {
+		return nil, err
+	}
+
+	return ssh.FixedHostKey(hostkey), nil
+}
+
+func getKeySigner(botPrivateKeyPath string) (ssh.Signer, error) {
+
+	var signer ssh.Signer
+
+	if botPrivateKeyPath == "" {
+		key, err := rsa.GenerateKey(rand.Reader, 2014)
+		if err != nil {
+			return nil, err
+		}
+
+		signer, err = ssh.NewSignerFromKey(key)
+		if err != nil {
+			return nil, err
+		}
+
+	} else {
+		key, err := ioutil.ReadFile(botPrivateKeyPath)
+		if err != nil {
+			return nil, err
+		}
+
+		signer, err = ssh.ParsePrivateKey(key)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return signer, nil
+}
+
+func getClientConfig(botName string, botKeyPath string, hostPubkeyPath string, allowInsecure bool) *ssh.ClientConfig {
+
+	hostkeyCallback, err := getHostPubkeyCallback(hostPubkeyPath, allowInsecure)
+	if err != nil {
+		log.Fatalf("FATAL: getHostPubkeyCallback %v", err)
+	}
+
+	if hostPubkeyPath == "" {
+		log.Println("warning: using ssh.InsecureIgnoreHostKey")
+	}
+
+	keySigner, err := getKeySigner(botKeyPath)
+	if err != nil {
+		log.Fatalf("FATAL: getKeySigner %v", err)
 	}
 
 	sshConfig := &ssh.ClientConfig{
-		User: NAME,
+		User: botName,
 		Auth: []ssh.AuthMethod{
-			ssh.PublicKeys(signer),
+			ssh.PublicKeys(keySigner),
 		},
-		HostKeyCallback: func(hostname string, remote net.Addr, key ssh.PublicKey) error {
-			return nil
-		},
+		HostKeyCallback: hostkeyCallback,
 	}
 
-	client, err := ssh.Dial("tcp", fmt.Sprintf("%v:%v", SERVER, PORT), sshConfig)
+	return sshConfig
+}
+
+func main() {
+
+	allowInsecureHostkey, err := strconv.ParseBool(ALLOW_INSECURE_HOSTKEY)
+	if err != nil {
+		log.Fatalf("failed to parse ALLOW_INSECURE_HOSTKEY into bool: %v", err)
+	}
+
+	historyRead := 0 // on connect, server sends last 20 lines to client - ignore these
+	historyLines, err := strconv.Atoi(HISTORY_PLAYBACK_LEN)
+	if err != nil {
+		log.Fatalf("failed to cast HISTORY_PLAYBACK_LEN to integer: %v", err)
+	}
+
+	sshConfig := getClientConfig(BOTNAME, BOT_PRIVATE_KEY, HOST_PUBLIC_KEY, allowInsecureHostkey)
+
+	client, err := ssh.Dial("tcp", fmt.Sprintf("%v:%v", HOST, PORT), sshConfig)
 	if err != nil {
 		log.Fatalf("unable to connect: %v", err)
 	}
@@ -53,8 +142,10 @@ func main() {
 	if err != nil {
 		log.Fatalln("unable to create stdin pipe to SSH session")
 	}
+	defer in.Close()
 
-	in.Write([]byte("/theme mono\r\n"))
+	greeting := fmt.Sprintf("/theme mono\r\n%v v%v: https://www.youtube.com/watch?v=SFLSOIufuhM\r\n", BOTNAME, VERSION)
+	in.Write([]byte(greeting))
 
 	out, err := session.StdoutPipe()
 	if err != nil {
@@ -63,8 +154,17 @@ func main() {
 
 	scanner := bufio.NewScanner(out)
 	for scanner.Scan() {
+		if historyRead < historyLines {
+			historyRead++
+			continue
+		}
 		line := scanner.Text()
-		log.Println(line)
+		log.Print(line)
+		time.Sleep(1 * time.Second)
+
+		if strings.Contains(line, fmt.Sprintf("hi %v", BOTNAME)) {
+			in.Write([]byte("hello\r\n"))
+		}
 	}
 
 	fmt.Println("connection closed")
